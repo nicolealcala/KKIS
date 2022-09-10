7544716037ccf6c5ea6f73c81b2b663b69547f94
John Marc Morano <jmcmorano@iskolarngbayan.pup.edu.ph>
2022-09-10 15:20:35 +0800

    KKISnav_backup_1920x1080

---
+++ KKISnav/1920x1080/assets/js/KKISnav.js
@@ -0,0 +1,20 @@
+//sidebar
+let sidebar = document.querySelector(".sidebar");
+
+btn.onclick = function() {
+    sidebar.classList.toggle("active");
+}
+
+//topnav
+// document.addEventListener(' DOMContentLoaded', init);
+
+// function init() {
+//     let query = window.watchMedia ("(max-width: 769px)");
+    
+//     if(query.matches) {
+//         document.querySelector('.sidebar').style.display = 'none';
+//     }else{
+//         document.querySelector('.sidebar').style.display = 'none';
+//     }
+// }
+
