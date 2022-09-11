9d2e6ef0a7d5b5deba8f7124f9f0c34b2bb67b20
nicolealcala <nicolealcalq@gmail.com>
2022-09-11 22:06:13 +0800

    Documents page nav update

--- assets/js/documents.js
+++ assets/js/documents.js
@@ -1,11 +1,1 @@
-// $(document).ready(function(){
-//      $("options").on('click', function() {
-//             $("options").removeClass("active");
-//             $(".options").addClass("active");
-//         });
-// });
-
-// function showPrinting() {
-//     let kkid = document.getElementByID("kkid");
-//     kkid.style.display="flex";
-// }
+// 
