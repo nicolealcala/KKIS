171831a3d521177cab984555f7026fbe19e9f7c3
nicolealcala <nicolealcalq@gmail.com>
2022-09-14 20:31:27 +0800

    Documents Nav fixed 98%
    Can't figure out how to change the style of an element within an active class

--- assets/js/documents.js
+++ assets/js/documents.js
@@ -1,4 +1,4 @@
-//Getting link id
+//Getting link id from document Nav
 const linkScholar = document.getElementById('linkReferral');
 linkScholar.addEventListener("click", documentReferral);
 
@@ -6,35 +6,49 @@ const linkID = document.getElementById('linkKkid');
 linkID.addEventListener("click", documentID);
 
 var kkidDiv = document.getElementById('kkidPage');
-
 kkidDiv.style.display='none';
 
 var referralDiv = document.getElementById('referralPage');
 
-//Jquery
-$(document).ready(function() {
-    $(".linkContainer").on('click', function(){
-        $(".linkContainer").removeClass("active");
-        $(this).addClass("active");
-    })
-});
+
+//declare variables needed for the loop
+var options = document.getElementsByClassName("linkContainer");
+
+// Loop through the divs and add the active class to the current/clicked div
+for (var i = 0; i < options.length; i++) {
+    options[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+//Declare variable for qrButton
+const qrButton = document.getElementById('qrScanner');
+qrButton.addEventListener("click", qrClicked);
+const qrIcon = document.getElementById('qrIcon');
 
 
 //Onclick functions
+//1. for Scholarship referral
 function documentReferral(){
    referralDiv.style.display='';
    kkidDiv.style.display='none';
-     document.getElementById('rowBtn').style.marginTop='30px';
-
+    document.getElementById('rowBtn').style.marginTop='30px';
 }
+//2. for KKID
 function documentID(){
    referralDiv.style.display='none';
     kkidDiv.style.display='';
     document.getElementById('rowBtn').style.marginTop='50px';
 }
+//3. for QR Code Scanning
+function qrClicked(){
+    qrButton.style.backgroundColor="black";
+    qrIcon.style.color="#F3CB69"
+}
 
-
-//for KKID
+//for disabling fields in KKID
 var purokChecker = document.getElementById('checkPurok');
 var purokOption = document.getElementById('selectPurok');
 purokChecker.onchange = function enableBtn() {
