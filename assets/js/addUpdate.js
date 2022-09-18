cd58c55a0672707d41d5b1e6714a62cab5548005
nicolealcala <nicolealcalq@gmail.com>
2022-09-18 18:05:16 +0800

    Updates

--- assets/js/addUpdate.js
+++ assets/js/addUpdate.js
@@ -1,36 +1,27 @@
 const eStatus = document.getElementsByClassName('statusCheck');
 var educStatus = document.getElementById('educCheck');
-// var educSelection = getElementsByClassName('educSelectBox');
-// var educInput = getElementsByClassName('educUserInput');
 var employStatus = document.getElementById('employCheck');
-var employSelection = document.querySelector("select.employSelectBox");
-// var employInput = getElementsByClassName('employUserInput');
 
-educStatus.onchange = function changeState() {
+educStatus.onchange = function changeStateEduc() {
     if(this.checked){
         employStatus.disabled = true;
-        document.getElementById('markerEmploy').style.background="#707070";
-        // employSelection.disabled = true;
-        // employInput.disabled = true;
-        
+        document.getElementById('markerEmploy').style.background="#a4a4a4";
+        document.getElementsByClassName('employSelectBox').setAttribute('disabled', 'disabled');
     } else {
         employStatus.disabled = false;
         document.getElementById('markerEmploy').style.background="#219EBC";
-        // employSelection.disabled = false;
-        // employInput.disabled = false;
+
     }
 };
 
 
-employStatus.onchange = function changeState() {
+employStatus.onchange = function changeStateEmploy() {
     if(this.checked){
         educStatus.disabled = true;
-        // educSelection.disabled = true;
-        // educInput.disabled = true;
+        document.getElementById('markerEduc').style.background="#a4a4a4";
     } else {
         educStatus.disabled = false;
-        // educSelection.disabled = false;
-        // educInput.disabled = false;
+        document.getElementById('markerEduc').style.background="#219EBC";
     }
 };
 
