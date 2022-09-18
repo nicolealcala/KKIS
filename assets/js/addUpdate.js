983cb4fab3806211f59c75ed128c25d6a705dce9
nicolealcala <nicolealcalq@gmail.com>
2022-09-18 21:22:05 +0800

    add entry js fixed

--- assets/js/addUpdate.js
+++ assets/js/addUpdate.js
@@ -2,26 +2,70 @@ const eStatus = document.getElementsByClassName('statusCheck');
 var educStatus = document.getElementById('educCheck');
 var employStatus = document.getElementById('employCheck');
 
-educStatus.onchange = function changeStateEduc() {
+//Employment Components:
+var selectEmployStatus = document.getElementById('selectEmployStatus');
+var selectType = document.getElementById('selectType');
+var selectCompanyType = document.getElementById('selectCompanyType');
+var selectEmploySalary = document.getElementById('selectEmploySalary');
+var inputEmployer = document.getElementById('inputEmployer');
+var inputEmployOccupation = document.getElementById('inputOccupation');
+
+
+//Education Components:
+var selectEducStatus = document.getElementById('selectEducStatus');
+var selectLevel = document.getElementById('selectLevel');
+var selectSchoolType = document.getElementById('selectSchoolType');
+var selectEducSalary = document.getElementById('selectEducSalary');
+var inputSchool = document.getElementById('inputSchool');
+var inputEducOccupation = document.getElementById('inputEducOccupation');
+var employFieldLabel = document.getElementsByClassName('employFieldLabel');
+
+
+
+educStatus.onchange = function changeState() {
     if(this.checked){
         employStatus.disabled = true;
-        document.getElementById('markerEmploy').style.background="#a4a4a4";
-        document.getElementsByClassName('employSelectBox').setAttribute('disabled', 'disabled');
+        document.getElementById('markerEmploy').style.background="#c5d4e3";
+        selectEmployStatus.disabled = true;
+        selectType.disabled = true;
+        selectCompanyType.disabled = true;
+        inputEmployer.disabled = true;
+        inputEmployOccupation.disabled = true;
+        selectEmploySalary.disabled = true;
+        employFieldLabel.style.color = "red";
+
+        
     } else {
         employStatus.disabled = false;
         document.getElementById('markerEmploy').style.background="#219EBC";
-
+        selectEmployStatus.disabled = false;
+        selectType.disabled = false;
+        selectCompanyType.disabled = false;
+        inputEmployer.disabled = false;
+        inputEmployOccupation.disabled = false;
+        selectEmploySalary.disabled = false;
     }
 };
 
-
-employStatus.onchange = function changeStateEmploy() {
+employStatus.onchange = function changeState() {
     if(this.checked){
         educStatus.disabled = true;
-        document.getElementById('markerEduc').style.background="#a4a4a4";
+        document.getElementById('markerEduc').style.background="#c5d4e3";
+        selectEducStatus.disabled = true;
+        selectLevel.disabled = true;
+        selectSchoolType.disabled = true;
+        inputSchool.disabled = true;
+        inputEducOccupation.disabled = true;
+        selectEducSalary.disabled = true;
     } else {
         educStatus.disabled = false;
         document.getElementById('markerEduc').style.background="#219EBC";
+        selectEducStatus.disabled = false;
+        selectLevel.disabled = false;
+        selectSchoolType.disabled = false;
+        inputSchool.disabled = false;
+        inputEducOccupation.disabled = false;
+        selectEducSalary.disabled = false;
     }
 };
 
