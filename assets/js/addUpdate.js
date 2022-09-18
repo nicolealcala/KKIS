543e3aad2afbd27b25ec0091de532f8bfa5c0fe3
nicolealcala <nicolealcalq@gmail.com>
2022-09-18 22:26:09 +0800

    Add entry js updated

--- assets/js/addUpdate.js
+++ assets/js/addUpdate.js
@@ -1,49 +1,42 @@
-const eStatus = document.getElementsByClassName('statusCheck');
-var educStatus = document.getElementById('educCheck');
-var employStatus = document.getElementById('employCheck');
-
 //Employment Components:
-var selectEmployStatus = document.getElementById('selectEmployStatus');
-var selectType = document.getElementById('selectType');
-var selectCompanyType = document.getElementById('selectCompanyType');
-var selectEmploySalary = document.getElementById('selectEmploySalary');
-var inputEmployer = document.getElementById('inputEmployer');
-var inputEmployOccupation = document.getElementById('inputOccupation');
-
+var educStatus = document.getElementById('educCheck');
+var employSelection = document.querySelectorAll('.employSelectBox');
+var employInput = document.querySelectorAll('.employUserInput');
+var employLabel = document.querySelectorAll('.employFieldLabel');
 
 //Education Components:
-var selectEducStatus = document.getElementById('selectEducStatus');
-var selectLevel = document.getElementById('selectLevel');
-var selectSchoolType = document.getElementById('selectSchoolType');
-var selectEducSalary = document.getElementById('selectEducSalary');
-var inputSchool = document.getElementById('inputSchool');
-var inputEducOccupation = document.getElementById('inputEducOccupation');
-var employFieldLabel = document.getElementsByClassName('employFieldLabel');
+var employStatus = document.getElementById('employCheck');
+var educSelection = document.querySelectorAll('.educSelectBox');
+var educInput = document.querySelectorAll('.educUserInput')
+var educLabel = document.querySelectorAll('.educFieldLabel');
 
-
-
+//On Change fxns
 educStatus.onchange = function changeState() {
     if(this.checked){
         employStatus.disabled = true;
         document.getElementById('markerEmploy').style.background="#c5d4e3";
-        selectEmployStatus.disabled = true;
-        selectType.disabled = true;
-        selectCompanyType.disabled = true;
-        inputEmployer.disabled = true;
-        inputEmployOccupation.disabled = true;
-        selectEmploySalary.disabled = true;
-        employFieldLabel.style.color = "red";
-
-        
+        employSelection.forEach(employSelect => {
+            employSelect.disabled = true;
+        });
+        employInput.forEach(employWrite => {
+            employWrite.disabled = true;
+        });
+        employLabel.forEach(label => {
+            label.style.color = '#cccccc';
+        });
+           
     } else {
         employStatus.disabled = false;
         document.getElementById('markerEmploy').style.background="#219EBC";
-        selectEmployStatus.disabled = false;
-        selectType.disabled = false;
-        selectCompanyType.disabled = false;
-        inputEmployer.disabled = false;
-        inputEmployOccupation.disabled = false;
-        selectEmploySalary.disabled = false;
+        employSelection.forEach(employSelect => {
+            employSelect.disabled = false;
+        });
+        employInput.forEach(employWrite => {
+            employWrite.disabled = false;
+        });
+        employLabel.forEach(label => {
+            label.style.color = '#909090';
+        });
     }
 };
 
@@ -51,20 +44,26 @@ employStatus.onchange = function changeState() {
     if(this.checked){
         educStatus.disabled = true;
         document.getElementById('markerEduc').style.background="#c5d4e3";
-        selectEducStatus.disabled = true;
-        selectLevel.disabled = true;
-        selectSchoolType.disabled = true;
-        inputSchool.disabled = true;
-        inputEducOccupation.disabled = true;
-        selectEducSalary.disabled = true;
+        educSelection.forEach(educSelect => {
+            educSelect.disabled = true;
+        });
+        educInput.forEach(educWrite => {
+            educWrite.disabled = true;
+        });
+        educLabel.forEach(label => {
+            label.style.color = '#cccccc';
+        });
     } else {
         educStatus.disabled = false;
         document.getElementById('markerEduc').style.background="#219EBC";
-        selectEducStatus.disabled = false;
-        selectLevel.disabled = false;
-        selectSchoolType.disabled = false;
-        inputSchool.disabled = false;
-        inputEducOccupation.disabled = false;
-        selectEducSalary.disabled = false;
+        educSelection.forEach(educSelect => {
+            educSelect.disabled = false;
+        });
+        educInput.forEach(educWrite => {
+            educWrite.disabled = false;
+        });
+        educLabel.forEach(label => {
+            label.style.color = '#909090';
+        });
     }
 };
