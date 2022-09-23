7f6fbd0f6d7fc680587c7bc2bcb72bc06e97402c
nicolealcala <nicolealcalq@gmail.com>
2022-09-23 21:16:36 +0800

    Added favicon to my pages

--- assets/js/declaredHousehold.js
+++ assets/js/declaredHousehold.js
@@ -1,14 +1,17 @@
 //for clicking Overview
 $(document).ready(function(){
     $('#families').on('click', function(){
-        $('#familiesBody').removeClass('d-none');
-        $('#overviewBody').addClass('d-none');
+        $('.familiesBody').removeClass('d-none');
+        $('.overviewBody').addClass('d-none');
     })
     
     
     $('#overview').on('click', function(){
-        $('#overviewBody').removeClass('d-none');
-        $('#familiesBody').addClass('d-none');
+        $('.overviewBody').removeClass('d-none');
+        $('.familiesBody').addClass('d-none');
     })
 })
+
+
+
 
