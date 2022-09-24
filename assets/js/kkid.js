b0c22e7083543c7b9a63f6042df60fe3044efc20
nicolealcala <nicolealcalq@gmail.com>
2022-09-25 00:54:32 +0800

    KK ID Page JS 100%

--- assets/js/kkid.js
+++ assets/js/kkid.js
@@ -1,38 +1,6 @@
-//for disabling categories in KKID
-$(document).ready(function(){
-    $('#checkPurok').on('change', function(){
-        if ($(this).is(':checked')){
-            $('#selectPurok').prop('disabled', false);
-        } else {
-            $('#selectPurok').prop('disabled', true);
-        }
-    })
-    
-})
 
-$(document).ready(function(){
-    $('#checkAge').on('change', function(){
-        if ($(this).is(':checked')){
-            $('#selectAge').prop('disabled', false);
-        } else {
-            $('#selectAge').prop('disabled', true);
-        }
-    })
-    
-})
+var checkCount = 0;
 
-$(document).ready(function(){
-    $('#checkCustom').on('change', function(){
-        if ($(this).is(':checked')){
-            $('.customForm').removeClass('d-none');
-        } else {
-            $('.customForm').addClass('d-none');
-        }
-    })
-    
-})
-
- 
 //For QR Scanner Button
 $(document).ready(function(){
     $('.qrBtn').on('click', function(){
@@ -43,26 +11,78 @@ $(document).ready(function(){
     })
 })
 
-const checkCount = 0;
-
-function weh (){
-    $('.checkboxCategory').on('change', function(){
-        if ($(this).is(':checked')){
-            checkCount+=1;
-        }
-    });
-}
-
+//For disabling categories in KKID
 $(document).ready(function(){
-    $('#submitBtn').on('click', function(){
-        if (checkCount==0){
-            alert('Naur');
+    $('#checkAge').on('change', function(){
+        if ($(this).is(':checked')){
+            $('#selectAge').prop('disabled', false);
+            checkCount++;
+            submitBtnState();
+            $('#checkCustom').prop('disabled', true);
         } else {
-            alert('woah');
+            $('#selectAge').prop('disabled', true);
+            checkCount--;
+            submitBtnState();
+            $('#checkCustom').prop('disabled', false);
         }
     })
+});
+
+$(document).ready(function(){
+    $('#checkPurok').on('change', function(){
+        if ($(this).is(':checked')){
+            $('#selectPurok').prop('disabled', false);
+            checkCount++;
+            submitBtnState();
+            $('#checkCustom').prop('disabled', true);
+        } else {
+            $('#selectPurok').prop('disabled', true);
+            checkCount--;
+            submitBtnState();
+            $('#checkCustom').prop('disabled', false);
+        }
+    })
+    
 })
 
-
-
+$(document).ready(function(){
+    $('#checkCustom').on('change', function(){
+        if ($(this).is(':checked')){
+            checkCount++;
+            submitBtnState();
+            $('#clearBtn').removeAttr("disabled");
+            $('.customDiv').removeClass('d-none');
+            $('.checkboxCategory').not(this).prop('checked', false); 
+            $('.checkboxCategory').not(this).prop('disabled', true); 
+        } else {
+            checkCount--;
+            submitBtnState();
+            $('.customDiv').addClass('d-none');
+            $('.checkboxCategory').not(this).prop('disabled', false); 
+        }
+    })
     
+})
+
+//for clicking Clear Btn
+$(document).ready(function(){
+    $('#clearBtn').on('click', function(){
+        $('.userInput').each(function(){
+            $(this).val("");
+        });
+    });
+})
+
+//fxn for changing submit state
+var submitBtnState = function (){
+    if (checkCount!=0){
+        $('#submitBtn').removeAttr("disabled", "disabled");
+    } else {
+        $('#submitBtn').attr("disabled", "disabled");
+    }
+
+    // $('#submitBtn').on('click', function(){
+    //     alert(checkCount);
+    // })
+}
+
