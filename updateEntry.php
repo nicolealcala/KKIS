44570a6029ee14f975e06fd42a368c5dc3a47e73
John Marc Morano <jmcmorano@iskolarngbayan.pup.edu.ph>
2022-09-26 19:52:15 +0800

    Update updateEntry.php

--- updateEntry.php
+++ updateEntry.php
@@ -15,7 +15,7 @@
     <link rel="stylesheet" href="assets/scss/addUpdate.css">
     <link rel="stylesheet" href="assets/scss/mediaquery.css">
     <link rel="stylesheet" href="assets/css/modal.compiled.css">
-    <link rel="stylesheet" href="assets/css/navAddUpdate.css">
+    <link rel="stylesheet" href="assets/css/navUpdateEntry.css">
         
     <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
     
