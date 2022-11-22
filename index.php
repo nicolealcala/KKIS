62b4009ced05f683abe051268a748daf75a7e582
nicolealcala <nicolealcalq@gmail.com>
2022-11-23 02:00:45 +0800

    Index login redirect fixed.

--- index.php
+++ index.php
@@ -28,11 +28,12 @@
     if(isset($_POST['loginBtn'])) {
         $email = $_POST['email'];
         $password = $_POST['password'];
+        $password_hash = password_hash($password, PASSWORD_DEFAULT);
         //$hash = password_hash($password, PASSWORD_DEFAULT);
     
-        $query = "SELECT * FROM `login_credentials` WHERE email = '".$email."' AND password = '".$password."'";
+        $query = "SELECT * FROM `login_credentials` WHERE email = '".$email."'";
         $accounts = executeQuery($query);
-    
+
         if(mysqli_num_rows($accounts) > 0) {
             while($account = mysqli_fetch_assoc($accounts)) {
                 if(password_verify($password,$account['password'])){
@@ -42,7 +43,7 @@
                     $_SESSION['contactNumber'] = $account['contactNumber'];
                     $_SESSION['email'] = $account['email'];
     
-                    header('Location: dashboard.php');
+                    header('Location: dashboard.php');  
                     //echo "<script>window.location.href='dashboard.php';</script>";
                    } else {
                         echo '<script>alert("Invalid email/password!")</script>';
