eec758c9b46909dd287226399a03cf4f5b7e0005
abrigopm <111882858+abrigopm@users.noreply.github.com>
2022-11-30 18:38:46 +0800

    separate php codes for login/signup
    para mas malinis lang

--- index.php
+++ index.php
@@ -1,57 +1,14 @@
 <?php
+// XAMPP
+
   include 'connection.php';
 
   session_start();
   session_destroy();
   session_start();
 
-  //SIGN UP not final pa ito pero connected na sa db yung signup
-  if(isset($_POST['signupBtn'])) {
-    $firstName = $_POST['firstName'];
-    $lastName = $_POST['lastName'];
-    $birthday = $_POST['birthday'];
-    $contactNumber = $_POST['contactNumber'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $password_hash = password_hash($password, PASSWORD_DEFAULT);
+  include 'controller.php';
 
-    $query = "INSERT INTO `login_credentials`(`firstName`, `lastName`, `birthday`, `contactNumber`, `email`, `password`) VALUES ('".$firstName."','".$lastName."','".$birthday."','".$contactNumber."','".$email."','".$password_hash."')";
-    if(executeQuery($query)){
-      header('Location: index.php');
-    }
-  } 
-
-    // LOGIN BTN 
-    // $Query_email = "email";
-    // $Query_password;
-
-    if(isset($_POST['loginBtn'])) {
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-        $password_hash = password_hash($password, PASSWORD_DEFAULT);
-        //$hash = password_hash($password, PASSWORD_DEFAULT);
-    
-        $query = "SELECT * FROM `login_credentials` WHERE email = '".$email."'";
-        $accounts = executeQuery($query);
-
-        if(mysqli_num_rows($accounts) > 0) {
-            while($account = mysqli_fetch_assoc($accounts)) {
-                if(password_verify($password,$account['password'])){
-                    $_SESSION['firstName'] = $account['firstName'];
-                    $_SESSION['lastName'] = $account['lastName'];
-                    $_SESSION['birthday'] = $account['birthday'];
-                    $_SESSION['contactNumber'] = $account['contactNumber'];
-                    $_SESSION['email'] = $account['email'];
-    
-                    header('Location: dashboard.php');  
-                    //echo "<script>window.location.href='dashboard.php';</script>";
-                   } else {
-                        echo '<script>alert("Invalid email/password!")</script>';
-                   }
-            }     
-        } 
-    }
-        
 ?>
 
 <!DOCTYPE html>
@@ -200,13 +157,13 @@
                         <!-- Fields -->
                         <div class="row gx-3 gy-3 mx-2">
                             <div class="col-12">
-                                <input class="form-control" type="text" placeholder="Username" required>
+                                <input class="form-control" type="email" placeholder="Email" required>
                             </div>
                             <div class="col-12">
-                                <input class="form-control" type="password" placeholder="New Password" required>
+                                <input class="form-control" type="password" name="newPassword placeholder="New Password" required>
                             </div>
                             <div class="col-12">
-                                <input class="form-control" type="password" placeholder="Confirm New Password" required>
+                                <input class="form-control" type="password" name="confirmPassword" placeholder="Confirm New Password" required>
                             </div>
                         </div>
                         <!-- Reset Pass btn -->
