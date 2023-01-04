7f9e9d56de21c5ffa087fa74968272312b3cc12b
nicolealcala <nicolealcalq@gmail.com>
2023-01-04 15:28:22 +0800

    Settings pages

---
+++ settings-FAQs.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FAQs</title>
+
+    <!-- Fundamental links -->
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
+
+    <!-- Custom Stylesheets -->
+    <link rel="stylesheet" href="assets/scss/settings.css">
+    <link rel="stylesheet" href="assets/scss/mediaquery.css">
+
+    <!-- DATA TABLES CDN -->
+    <link rel="stylesheet" href="assets/css/datatables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.4.0/css/responsive.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.7.1/css/buttons.dataTables.min.css">
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="assets/img/logos/favicon.ico">
+</head>
+
+<body>
+    <div class="sidePanel d-none d-lg-flex">
+        <?php
+        include "settings.php";
+        ?>
+    </div>
+
+    <div class="mainPanel">
+        <div class="container-fluid content">
+            <i role="button" class="backIcon fa-solid fa-arrow-left me-3 d-lg-none"></i>
+            <span class="mainTitle">FAQs</span>
+
+            <div class="sectionDiv">
+               <!-- CONTENT STARTS HERE -->
+            </div>
+
+
+        </div>
+    </div>
+
+
+    <!-- Fundamental Links -->
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Data tables -->
+    <script src="assets/js/datatables.min.js"></script>
+    <script src="assets/js/pdfmake.min.js"></script>
+    <script src="assets/js/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.4.0/js/dataTables.responsive.min.js"></script>
+
+    <!-- Custom Script -->
+    <script type="text/javascript" src="assets/js/settings.js"></script>
+</body>
+
+</html>
