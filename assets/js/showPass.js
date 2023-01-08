914a9a53b808939cf00f9dca18b3f23e9708a935
abrigopm <111882858+abrigopm@users.noreply.github.com>
2023-01-08 16:07:41 +0800

    change btn for showpass

--- assets/js/showPass.js
+++ assets/js/showPass.js
@@ -1,12 +1,24 @@
-function showPassword() {
-    // body...
-    var user_password=document.getElementById('user_password');
-    if(user_password.type=='password')
-    {
-        user_password.type='text';
+// function showPassword() {
+//     // body...
+//     var user_password=document.getElementById('user_password');
+//     if(user_password.type=='password')
+//     {
+//         user_password.type='text';
+//     }
+//     else
+//     {
+//         user_password.type='password';
+//     }
+// }
+
+$(".toggle-password").click(function() {
+
+    $(this).toggleClass("fa-eye fa-eye-slash");
+    var input = $($(this).attr("toggle"));
+    if (input.attr("type") == "password") {
+      input.attr("type", "text");
+    } else {
+      input.attr("type", "password");
     }
-    else
-    {
-        user_password.type='password';
-    }
-}
+});
+
